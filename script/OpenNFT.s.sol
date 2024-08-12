420e3b84ac4d1da31e74ec2b9d47ca52b7b72566
tim <18652961108@163.com>
2024-08-12 15:21:01 +0800

    flashbotbundle

--- script/OpenNFT.s.sol
+++ script/OpenNFT.s.sol
@@ -2,14 +2,14 @@
 pragma solidity ^0.8.20;
 
 import "forge-std/Script.sol";
-import "../src/OpenNFT.sol";
+import {OpenNFT} from "../src/OpenNFT.sol";
 
-contract DeployOpenspaceNFT is Script {
+contract DeployOpenNFT is Script { 
     function run() external {
         vm.startBroadcast();
         
         // 部署合约
-        OpenspaceNFT nft = new OpenspaceNFT();
+        OpenNFT nft = new OpenNFT();
         
         // 打印合约地址
         console.log("OpenspaceNFT deployed to:", address(nft));
